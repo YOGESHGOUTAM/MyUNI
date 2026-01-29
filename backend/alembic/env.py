76b30e847721fdcfc0d6e06eeb3e76737bd48a55
YOGESHGOUTAM <avengersinmars@gmail.com>
2026-01-29 20:45:28 +0530

    change in env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -72,7 +72,7 @@ def run_migrations_online() -> None:
     connectable = engine_from_config(
         config_section,
         prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+        poolclass=pool.NullPool
     ),
     with connectable.connect() as connection:
         context.configure(
